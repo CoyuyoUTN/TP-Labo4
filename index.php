cafb6b9fd72dc557aeb94b74fcf589e4dea38bcf
Matias Ezequiel Querejeta <matiasezequielquerejeta@gmail.com>
2021-09-28 10:39:32 -0300

    Cambios de Titulos
    Agregue cambios en los titulos y nombres para seguir testeando el GitHub

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>Bienvenidos al <br>Proyecto Final de Lab 4</title>
 </head>
 <body>
   <h1>Bienvenido <?php echo $email; ?>!</h1>
-  <a href="logout.php">Cerrar sesión</a>
+  <a href="logout.php">Logout</a>
 </body>
 </html>
