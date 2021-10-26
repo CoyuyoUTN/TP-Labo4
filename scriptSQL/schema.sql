9753c9ac4dd4ce0fc1824e3411a9174e99a0cf47
Matias Ezequiel Querejeta <matiasezequielquerejeta@gmail.com>
2021-10-26 10:54:02 -0300

    Agregamos las tablas
    Agregamos las tablas a la base de datos

--- scriptSQL/schema.sql
+++ scriptSQL/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE company (
+CREATE TABLE proyecto4.Company (
   name varchar(30) NOT NULL,
   cuil int(20) NOT NULL,
   id int(20) NOT NULL,
@@ -15,6 +15,31 @@ CREATE TABLE company (
   webpage varchar(30) NOT NULL,
   facebook varchar(30) NOT NULL
 ),
+CREATE TABLE proyecto4.JobsOffer (
+	id INT auto_increment NOT NULL,
+	Description varchar(100) NOT NULL,
+	CompanyId INT NOT NULL,
+	JobPositionId INT NOT NULL,
+	CONSTRAINT JobsOffer_PK PRIMARY KEY (id),
+	CONSTRAINT JobsOffer_FK FOREIGN KEY (CompanyId) REFERENCES proyecto4.Company(id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+AUTO_INCREMENT=1;
+CREATE TABLE Career (
+  careerId int(11) NOT NULL AUTO_INCREMENT,
+  description varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  active tinyint(1) NOT NULL,
+  PRIMARY KEY (careerId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE Student (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  Password varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  CareerId varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE proyecto4.Admin (
 	Id INT auto_increment NOT NULL,
@@ -23,7 +48,18 @@ CREATE TABLE proyecto4.Admin (
 	Name varchar(30) NOT NULL,
 	CONSTRAINT Id_PK PRIMARY KEY (Id),
 	CONSTRAINT Email_UN UNIQUE KEY (Email)
+) 
+CREATE TABLE proyecto4.Student_x_JobOffer (
+	StudentId INT NOT NULL,
+	JobOfferId INT NOT NULL,
+	CONSTRAINT Student_x_JobOffer_PK PRIMARY KEY (StudentId,JobOfferId),
+	CONSTRAINT Student_x_JobOffer_FK FOREIGN KEY (StudentId) REFERENCES proyecto4.Student(id),
+	CONSTRAINT Student_x_JobOffer_FK_1 FOREIGN KEY (JobOfferId) REFERENCES proyecto4.JobsOffer(id)
 )
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
+
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
