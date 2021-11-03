349e8564e897b42359ef1c83067cf828f948112a
Santiago Paak <santiago.paak@gmail.com>
2021-11-03 00:30:25 -0300

    add: Agregar nueva oferta laboral

--- Views/offersList.php
+++ Views/offersList.php
@@ -1,56 +1,60 @@
 <?php
-     use DAO\NavDAO as NavDAO;
-     NavDAO::getNav();
-     require_once("validate-session.php");
+
+use DAO\NavDAO as NavDAO;
+
+NavDAO::getNav();
+require_once("validate-session.php");
 ?>
 
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
                <h2 class="mb-4">Listado de Ofertas</h2>
-               <form action="<?php echo FRONT_ROOT."Offers/AddOffer" ?>"  method="">
+               <a class="btn btn-default" href="AddForm" >Nueva Oferta</a>
+               <br /><br />
+               <form action="<?php echo FRONT_ROOT . "Offers/AddOffer" ?>" method="">
 
-               <button type=submit >Buscar</button>
+                    <button type=submit>Buscar</button>
 
-               <br />
-               <br />
-               
-               <table class="table bg-light-alpha" border="3">
-                    <thead>
-                        <th>
-                              ID
-                              <input class="form-control" type="text" id="id" name="id">
-                         </th>
-                        <th>
-                             Descripcion
-                             <input class="form-control" type="text" id="description" name="description">
-                         </th>
-                        <th>
-                             Compañia
-                             <input class="form-control" type="text" id="company" name="company">
-                         </th>
-                        <th>
-                             Posicion
-                             <input class="form-control" type="text" id="position" name="position">
-                         </th>
-                    </thead>
-                    <tbody>
-                         <?php
-                              foreach($offers as $offer)
-                              {
-                                   ?>
-                                        <tr>
-                                             <td><?php echo $offer->getId() ?></td>
-                                             <td><?php echo $offer->getDescription() ?></td>
-                                             <td><?php echo $offer->getCompanyId() ?></td>
-                                             <td><?php echo $offer->getJobPosition()->getDescription() ?></td>
-                                        </tr>
-                                   <?php
+                    <br />
+                    <br />
+
+                    <table class="table bg-light-alpha" border="3">
+                         <thead>
+                              <th>
+                                   ID
+                                   <input class="form-control" type="text" id="id" name="id">
+                              </th>
+                              <th>
+                                   Descripcion
+                                   <input class="form-control" type="text" id="description" name="description">
+                              </th>
+                              <th>
+                                   Compañia
+                                   <input class="form-control" type="text" id="company" name="company">
+                              </th>
+                              <th>
+                                   Posicion
+                                   <input class="form-control" type="text" id="position" name="position">
+                              </th>
+                         </thead>
+                         <tbody>
+                              <?php
+                              foreach ($offers as $offer) {
+                              ?>
+                                   <tr>
+                                        <td><?php echo $offer->getId() ?></td>
+                                        <td><?php echo $offer->getDescription() ?></td>
+                                        <td><?php echo $this->companyDao->read($offer->getCompanyId())->getName() ?></td>
+                                        <td><?php echo $offer->getJobPosition()->getDescription() ?></td>
+                                   </tr>
+                              <?php
                               }
-                         ?>
-                         </tr>
-                    </tbody>
-               </table></form>
+                              ?>
+                              </tr>
+                         </tbody>
+                    </table>
+               </form>
           </div>
      </section>
 </main>
---
+++ Views/offersNew.php
@@ -0,0 +1,40 @@
+<?php
+
+use DAO\NavDAO as NavDAO;
+
+NavDAO::getNav();
+require_once("validate-session.php");
+?>
+
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container">
+            <h2 class="mb-4">Listado de Ofertas</h2>
+            <form action="<?php echo FRONT_ROOT . "Offers/AddOffer" ?>" method="">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <label for="company">Compañia: </label>
+                        <a class="btn btn-default" name="company" href="../Company/ShowListView?onAction=Offers/AddForm&actionName=Elegir"><?php echo $company ?></a>
+                    </div>
+                    <div class="col-lg-12">
+                        <label for="position">Puesto: </label>
+                        <select name="position" id="position">
+                            <?php
+                            foreach ($this->jobPositionDao->getAll() as $position) {
+                            ?>
+                                <option value="<?php echo $position->getJobPositionId() ?>"><?php echo $position->getDescription() ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="col-lg-12">
+                        <label for="description">Descripcion</label>
+                        <textarea id="description" name="description" rows="4" cols="50" class="form-control"></textarea>
+                    </div>
+                </div>
+                <button type="submit" class="btn" name="companyId" value="<?php echo $id ?>" style="background-color:#DC8E47;color:white;" >Enviar</button>
+            </form>
+        </div>
+    </section>
+</main>
