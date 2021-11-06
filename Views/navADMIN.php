6b62fe6d0f7404a5b96f3354e8832dcd1e0ac442
Santiago Paak <santiago.paak@gmail.com>
2021-11-06 20:30:41 -0300

    add: Offers en admin

--- Views/navADMIN.php
+++ Views/navADMIN.php
@@ -1,35 +1,36 @@
-
-
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
      <span class="navbar-text">
           <strong>Framework</strong>
      </span>
      <ul class="navbar-nav ml-auto">
-          
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo  FRONT_ROOT."Company/ShowListView "?>">Listar empresas</a>
-          </li>    
-     
-        
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo  FRONT_ROOT."Home/ShowListView "?>">Listar Alumnos</a>
-          </li>   
-     
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo  FRONT_ROOT."Admin/ShowAll "?>">Listar Admins</a>
-          </li>   
-          
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo  FRONT_ROOT."Home/Logout "?>">Cerrar sesion</a>
-          </li> 
-                      
-     </ul>
-   
 
-  
-    
-     
-       
-                    
-                   
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo  FRONT_ROOT . "Offers/ShowAll " ?>">Listar ofertas</a>
+          </li>
+
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo  FRONT_ROOT . "Company/ShowListView " ?>">Listar empresas</a>
+          </li>
+
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo  FRONT_ROOT . "Home/ShowListView " ?>">Listar Alumnos</a>
+          </li>
+
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo  FRONT_ROOT . "Admin/ShowAll " ?>">Listar Admins</a>
+          </li>
+
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo  FRONT_ROOT . "Home/Logout " ?>">Cerrar sesion</a>
+          </li>
+
+     </ul>
+
+
+
+
+
+
+
+
 </nav>
