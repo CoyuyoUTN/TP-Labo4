04065e54fd63498f6ee981b489f584fdd22c7173
Matias Ezequiel Querejeta <matiasezequielquerejeta@gmail.com>
2021-11-09 00:36:00 -0300

    Update Details

--- Controllers/CompanyController.php
+++ Controllers/CompanyController.php
@@ -20,7 +20,7 @@ class CompanyController
         require_once(VIEWS_PATH . "companyADD.php");
     }
 
-    public function ShowListView()
+    public function ShowListView($onAction = "Company/Remove",$actionName = "Eliminar")
     {
         $companyList=null;
         if (isset($_GET['search'])) {
@@ -42,6 +42,7 @@ class CompanyController
         require_once(VIEWS_PATH . "validate-session.php");
         require_once(VIEWS_PATH . "companyList.php");
     }
+    
 
     public function Add($name, $cuil, $shortDesc = null, $ranking = null, $email = null, $phone = null, $city = null, $address = null, $jobOffers = null, $linkedin = null, $webpage = null, $facebook = null, $img = null, $bio = null)
     {
