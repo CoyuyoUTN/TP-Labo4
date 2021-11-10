92935c1b2a0a5947c6fa1f003d6fb4b6d18d15fb
Gonzalo <herrero_gonza@hotmail.com>
2021-11-10 20:55:09 -0300

    comentarios en interfaz y admin dao

--- DAO/Crud.php
+++ DAO/Crud.php
@@ -6,11 +6,11 @@
  */
 interface Crud
 {
-	public function create($object);
-	public function read($id);
-	public function readAll();
-	public function update($object);
-	public function delete($object);
+	public function create($object); // Recibe un objeto y lo carga a su respectiva tabla en la base de datos.
+	public function read($id); // Retorna un objeto de una tabla en particular pasando por parametro como atributo de busqueda un numero Id.
+	public function readAll(); // Retorna una lista con todos los datos de una tabla en particular.
+	public function update($object);// recibe un objeto por parametro y modifica los datos en su respectiva tabla en la base de datos.
+	public function delete($object); // Elimina un objeto en su respectiva tabla en la base de datos.
 }
 
 ?>
